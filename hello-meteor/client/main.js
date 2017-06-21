64e0af00c43e02fa24f952771135ba8e24b3b16e
MichaelWiss <michael.wiss@gmail.com>
2017-06-21 14:07:45 -0500

    name variable

--- hello-meteor/client/main.js
+++ hello-meteor/client/main.js
@@ -1,3 +1,5 @@
-import {greetUser} from './../imports/utils';
+import {greetUser, name} from './../imports/utils';
 
 console.log('Log from /client/main.js');
+console.log(greetUser());
+console.log(name);
--- hello-meteor/imports/utils.js
+++ hello-meteor/imports/utils.js
@@ -3,3 +3,5 @@ console.log('Log from /imports/utils.js');
 export let greetUser = function () {
    return 'Hello User!'
 };
+
+export let name = 'Michael Wiss';
