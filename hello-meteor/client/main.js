aa9ec5e2196609f07631bee0fafaba347ed71046
MichaelWiss <michael.wiss@gmail.com>
2017-06-22 12:50:50 -0500

    title variable

--- hello-meteor/client/main.js
+++ hello-meteor/client/main.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function () {
+	let title = 'Account Settings'
 	let name = 'Michael';
-	let jsx = <p>Hello {name}!</p>;
+	let jsx = (
+		<div>
+		<h1> {title} </h1>
+	     <p>Hello {name}!</p>
+	     <p>This is my second P.</p>
+	   </div>
+	 );
 	ReactDOM.render(jsx, document.getElementById('app'));
 });
