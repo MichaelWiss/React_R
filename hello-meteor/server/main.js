d3ec9b239aa06cb1b1785fa4ad11260ebafb5bd5
MichaelWiss <michael.wiss@gmail.com>
2017-06-22 18:46:34 -0500

    save player

--- hello-meteor/server/main.js
+++ hello-meteor/server/main.js
@@ -1,1 +1,10 @@
+import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
+
+Meteor.startup(function() {
+    Players.insert({
+    	name: 'Michael',
+    	score: 3
+    });
+    console.log(Players.find().fetch());
+});
