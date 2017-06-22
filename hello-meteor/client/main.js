96d270ac98980755f3b266c2c77da144d846c99f
MichaelWiss <michael.wiss@gmail.com>
2017-06-22 13:34:00 -0500

    players array

--- hello-meteor/client/main.js
+++ hello-meteor/client/main.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+	_id: '1',
+	name: 'Lauren',
+	score: 99
+}, {
+	_id: '2',
+	name: 'Cory'
+	score: -1
+}, {
+	_id: '3',
+	name: 'Andrew',
+	score: -12
+}];
+
 Meteor.startup(function () {
-	let title = 'Account Settings'
+	let title = 'Account Settings';
 	let name = 'Michael';
 	let jsx = (
 		<div>
