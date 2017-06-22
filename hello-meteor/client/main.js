6d6564d5717633f8c7740f05841d1a527cd1d183
MichaelWiss <michael.wiss@gmail.com>
2017-06-22 15:21:18 -0500

    return an array

--- hello-meteor/client/main.js
+++ hello-meteor/client/main.js
@@ -16,6 +16,18 @@ const players = [{
 	score: -12
 }];
 
+const renderPlayers = function (playersList) {
+  let numbers = [{val:1}, {val:2}, {val:3}];
+
+  let newNumbers= numbers.map(function (number) {
+  	return number.val - 1;
+
+  });
+  console.log(newNumbers);
+
+  return [<p key="1">1</p>, <p key="2">2</p>, <p key="3">3</p>];
+};
+
 Meteor.startup(function () {
 	let title = 'Account Settings';
 	let name = 'Michael';
@@ -24,7 +36,7 @@ Meteor.startup(function () {
 		<h1>{title}</h1>
 	     <p>Hello {name}!</p>
 	     <p>This is my second P.</p>
-	     {[<p>1</p>, <p>2</p>]}
+	     {renderPlayers()}
 	   </div>
 	 );
 	ReactDOM.render(jsx, document.getElementById('app'));
