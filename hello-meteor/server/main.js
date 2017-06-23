c5170945428e48df4255aebf8013de65ed4fcd0b
MichaelWiss <michael.wiss@gmail.com>
2017-06-22 19:00:30 -0500

    add player

--- hello-meteor/server/main.js
+++ hello-meteor/server/main.js
@@ -2,9 +2,10 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(function() {
-    Players.insert({
-    	name: 'Michael',
-    	score: 3
+    Players.insert(
+    {
+    	name: 'Antoinette',
+    	score: 2
     });
     console.log(Players.find().fetch());
 });
