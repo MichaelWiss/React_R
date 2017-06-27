efae3e4e3849fc2c8d626b71bd115831a2c8e33c
MichaelWiss <michael.wiss@gmail.com>
2017-06-27 07:41:34 -0500

    class

--- hello-meteor/server/main.js
+++ hello-meteor/server/main.js
@@ -2,6 +2,16 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
-   
-  
+   class Person {
+     constructor(name = 'Anonymous') {
+         this.name = name;
+     }
+     getGreeting() {
+     	return 'Hi! I am ' + this.name + '.';
+     }
+   }
+
+
+  let me = new Person('Michael');
+   console.log(me);
 });
