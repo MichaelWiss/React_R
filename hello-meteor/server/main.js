975b19f5541b2adf7713af5a714681a619ee82cf
MichaelWiss <michael.wiss@gmail.com>
2017-06-27 07:51:34 -0500

    using class

--- hello-meteor/server/main.js
+++ hello-meteor/server/main.js
@@ -3,15 +3,19 @@ import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
    class Person {
-     constructor(name = 'Anonymous') {
+     constructor(name = 'Anonymous', age = '0') {
          this.name = name;
+         this.age = age;
      }
      getGreeting() {
-     	return 'Hi! I am ' + this.name + '.';
+     	// return 'Hi! I am ' + this.name + '.';
+     	return `Hi I am ${this.name}.`;
      }
    }
 
 
   let me = new Person('Michael');
-   console.log(me);
+   console.log(me.getGreeting());
+   let him = new Person('Andrew', 25);
+   console.log(him.getGreeting());
 });
