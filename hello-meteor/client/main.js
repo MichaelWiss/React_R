53b8f03bf4d7399d6caad28cbc429559022a5148
MichaelWiss <michael.wiss@gmail.com>
2017-06-28 14:06:36 -0500

    react component

--- hello-meteor/client/main.js
+++ hello-meteor/client/main.js
@@ -42,6 +42,17 @@ const handleSubmit = (e) => {
   }
 }; 
 
+
+class TitleBar extends React.Component {
+     render() {
+     	return (
+          <div>
+             <h1> My App Name</h1>
+          </div>
+     	);
+     }
+}
+
 Meteor.startup(() => {
    Tracker.autorun(() => {
 	  let players = Players.find().fetch();
