b87a5cd2287e7afa90522664a60d676f40e926ba
MichaelWiss <michael.wiss@gmail.com>
2017-06-30 18:54:00 -0500

    PlayerList

--- hello-meteor/imports/ui/PlayerList.js
+++ hello-meteor/imports/ui/PlayerList.js
@@ -2,22 +2,16 @@ import React from 'react';
 import Player from './player';
 
 
-const renderPlayers = (playersList) => {
-  return playersList.map((player) => {
-     return <Player key={player._id} player={player}/>;
-    });
- };
-
 export default class PlayerList extends React.Component {
-	renderPlayer() {
-    return this.props.map((player) => {
+	renderPlayers() {
+    return this.props.players.map((player) => {
      return <Player key={player._id} player={player}/>;
     });
-	}
-	render()  {
-      return (
+ }
+render()  {
+     return (
       	<div>
-      	  player list
+      	  {this.renderPlayers()}
       	</div>
       );
 	}
