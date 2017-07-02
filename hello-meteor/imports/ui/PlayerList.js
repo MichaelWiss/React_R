d0e5025b3f60d4fcd8cba65508111a1b88971b0a
MichaelWiss <michael.wiss@gmail.com>
2017-07-02 14:34:02 -0500

    if statement

--- hello-meteor/imports/ui/PlayerList.js
+++ hello-meteor/imports/ui/PlayerList.js
@@ -4,7 +4,7 @@ import Player from './player';
 export default class PlayerList extends React.Component {
 	renderPlayers() {
       if (this.props.players.length ===0) {
-
+      	return <p>Add a Player to Begin!</p>
       } else {
         return this.props.players.map((player) => {
           return <Player key={player._id} player={player}/>;
