23ff32c2eba2ac6da110a4038946192ecda01fac
MichaelWiss <michael.wiss@gmail.com>
2017-07-02 17:32:03 -0500

    app.js

--- hello-meteor/client/main.js
+++ hello-meteor/client/main.js
@@ -5,24 +5,13 @@ import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
 import App from './../imports/ui/app';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import Byline from './../imports/ui/Byline';
-import PlayerList from './../imports/ui/PlayerList'; 
+
 
 Meteor.startup(() => {
    Tracker.autorun(() => {
 	  let players = Players.find().fetch();
 	  let title = 'Score Keep';
-	  let jsx = (
-		<div>
-		 <TitleBar title={title}/>
-		 <Byline/>
-	     <PlayerList players={players}/>
-	     <AddPlayer/>
-	     </div>
-	 );
-	ReactDOM.render(jsx, document.getElementById('app'));
+	ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });  
 });
 
--- hello-meteor/imports/ui/app.js
+++ hello-meteor/imports/ui/app.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import TitleBar from './TitleBar';
+import AddPlayer from './AddPlayer';
+import Byline from './Byline';
+import PlayerList from './PlayerList'; 
+
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-			   app component
+			    <TitleBar title={this.props.title}/>
+		        <Byline byline={this.props.byline}/>
+	            <PlayerList players={this.props.players}/>
+	            <AddPlayer/>
 			</div>
         );
 	}
-}
+};
