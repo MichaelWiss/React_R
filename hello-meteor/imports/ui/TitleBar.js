94fe4876c0716569ce19b50971bd34bb11568e71
MichaelWiss <michael.wiss@gmail.com>
2017-07-03 19:35:38 -0500

    css

--- hello-meteor/imports/ui/TitleBar.js
+++ hello-meteor/imports/ui/TitleBar.js
@@ -11,7 +11,7 @@ export default class TitleBar extends React.Component {
      }
      render() {
      	return (
-          <div>
+          <div className="title-bar">
              <h1>{this.props.title}</h1>
              {this.renderSubtitle()}
 
